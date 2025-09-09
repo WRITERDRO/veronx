581a9ee76a4695d3301a0a78c105755f97f31343
WRITERDRO <devasking09@gmail.com>
2025-09-10 02:07:31 +0530

    Update post.py

--- ANNIEMUSIC/plugins/sudo/post.py
+++ ANNIEMUSIC/plugins/sudo/post.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from ANNIEMUSIC import app
 from config import OWNER_ID
 
-destination_group_id = -1002076109983
+destination_group_id = -1002333051623
 
 @app.on_message(filters.command(["post"], prefixes=["/", "."]) & filters.user(OWNER_ID))
 async def copy_messages(_, message):
